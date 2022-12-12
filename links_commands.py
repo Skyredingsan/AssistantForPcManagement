ecb8e249f23707bc5c90fdebece453215cd7a8af
Skyreding <Skyreding@mail.ru>
2022-12-12 14:39:18 +0500

    Fix bug with discord

--- links_commands.py
+++ links_commands.py
@@ -6,6 +6,6 @@ url = {'вк': 'vk.com/im',
        'twitch': 'twitch.tv'}
 
 # Открытие приложений
-app = {'steam': 'F:\Steam\steam.exe',
-       'discord': Path.home() / ('AppData\Local\Discord\app-1.0.9008\Discord.exe'),
-       'telegram': Path.home() / ('AppData\Roaming\Telegram Desktop\Telegram.exe')}
+app = {'steam': 'F:\\Steam\\steam.exe',
+       'discord': Path.home() / ('AppData\\Local\\Discord\\app-1.0.9008\\Discord.exe'),
+       'telegram': Path.home() / ('AppData\\Roaming\\Telegram Desktop\\Telegram.exe')}
